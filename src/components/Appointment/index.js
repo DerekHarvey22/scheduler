3c7699bacf3dd32e8ee8ec8df0900ae18ce2f4c7
Derek Harvey <derek.harvey0422@gmail.com>
2022-05-26 16:24:58 -0400

    create and implelemt hooks while creating tests

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,25 +4,34 @@ import 'components/Appointment/styles.scss';
 import Header from "./header";
 import Show from "./Show";
 import Empty from "./Empty"
-import { Fragment } from 'react';
+import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form"
+
+
+
 
 export default function Appointment(props) {
+
+  const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+const { mode, transition, back } = useVisualMode(
+  props.interview ? SHOW : EMPTY
+);
   return (
-    <Fragment>
-      <Header time={props.time} />
-      {props.interview ? 
-          <article className="appointment">
-        <Show
-          student={props.interview.student}
-          interviewer={props.interview.interviewer}
-        />
-        </article>
- :
- <article className="appointment"> 
-        <Empty />
-    
+<article className="appointment">
+    < Header time={props.time} />
+    {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+    {mode === SHOW && (
+      <Show
+        student={props.interview.student}
+        interviewer={props.interview.interviewer}
+      />
+    )}
+    {mode === CREATE && (
+      < Form
+        interviewers={props.interviewers} onCancel={() => back()} />
+    )}
     </article>
-    }
-    </Fragment>
   )
-};
+    }
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (newMode, replace = false) => {
+    if(replace) { 
+      setHistory([...history.slice(0, history.length-1), newMode]);
+      return setMode(newMode);
+    } 
+    
+    setMode(newMode);
+    
+    setHistory([...history, newMode]);
+    
+  }
+
+  const back = () => {
+    if (history.length === 1) 
+    return; 
+    setHistory([...history.slice(0, history.length -1)]);
+    
+    return setMode(history[history.length -2])
+  }
+  return { mode, transition, back };
+}
